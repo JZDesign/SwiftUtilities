971a1cae06d7da9f323955cc5e28fe0fb28777a2
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2020-08-22 09:17:02 -0500

    remove file loader, add docs, add tests

--- Sources/SwiftUtilities/Atomic.swift
+++ Sources/SwiftUtilities/Atomic.swift
@@ -2,8 +2,28 @@ import Foundation
 
 // Credits to Vadim Bulavin https://www.vadimbulavin.com/swift-atomic-properties-with-property-wrappers/
 
-@propertyWrapper
-public class Atomic<Value> {
+
+/// Atomic reads and writes via a property wrapper
+///
+/// **Note:**
+/// When performing simultaneous read/write operations like `+=`
+/// you must use the `mutate` function for atomic functionality
+///
+/// ```swift
+///
+/// @Atomic public var balance: Decimal
+///
+/// // Write without read works atomically as expected
+/// func resetBalance() { balance = 1000.00 }
+///
+/// // This does **NOT** guarantee atomic operations
+/// func incrementBalane() { balance += 500 }
+///
+/// // This does guarantee atomic operations
+/// func decrement() { $balance.mutate { $0 -= 500 } }
+/// ```
+///
+@propertyWrapper public class Atomic<Value> {
     private let queue = DispatchQueue(label: "com.jzdesign.atomic")
     private var value: Value
 
@@ -11,10 +31,7 @@ public class Atomic<Value> {
         self.value = wrappedValue
     }
 
-    public var projectedValue: Atomic<Value> {
-        return self
-    }
-
+    /// The value of the atomic wrapped object.
     public var wrappedValue: Value {
         get {
             return queue.sync { value }
@@ -24,8 +41,13 @@ public class Atomic<Value> {
         }
     }
 
+
+    /// Atomic mutation
+    /// - Parameter mutation: A mutating function used when getting and setting the wrapped value is necessary
+    ///
+    /// Atomic's `wrappedValue` is capable of atomic gets and sets, however when attempting to do both with operators like `+=` that is a nonatomic operation. It is for this reason that the mutate function exists
     public func mutate(_ mutation: (inout Value) -> Void) {
-        return queue.sync {
+        queue.sync {
             mutation(&value)
         }
     }
